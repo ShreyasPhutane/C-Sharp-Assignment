f9d39a0de6b48270bbea46209f502a3a19bf04bf
shreyasphutane17 <shreyasphutane17@gmail.com>
2022-10-13 17:49:03 +0530

    assignmet 2 commited

---
+++ assignment 2/Student_Regestration_Assignment2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Student_Regestration_Assignment2
+{
+    public partial class Frm_Student_Login : Form
+    {
+        public Frm_Student_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            if (((tb_Username.Text == "Admin") && (tb_Password.Text == "123")) || ((tb_Username.Text == "A") && (tb_Password.Text == "A")))
+            {
+                MessageBox.Show("Loginn Successsful", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                frm_Add_Student_Details obj = new frm_Add_Student_Details();
+                obj.Show();
+                this.Hide();
+            }
+            else
+            {
+                lbl_Error.Text = "Invalid Username Or Password";
+                lbl_Error.ForeColor = Color.OrangeRed;
+            }
+            tb_Username.Clear();
+            tb_Password.Clear();
+
+            tb_Password.Enabled = false;
+            btn_Submit.Enabled = false;
+
+            tb_Username.Focus();
+        }
+
+        private void tb_Username_TextChanged(Object Sender, EventArgs e)
+        {
+            lbl_Error.Visible = true;
+            tb_Password.Enabled = true;
+        }
+
+        private void tb_Password_TextChanged(object sender, EventArgs e)
+        {
+            btn_Submit.Enabled = true;
+        }
+    }
+}
